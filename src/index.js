75d9bff5295cc8031e3f60c288b8cdaffda562eb
cooljjjmarwa <cooljjjmarwa@gmail.com>
2023-04-19 12:21:35 +0300

    Completed module

--- src/index.js
+++ src/index.js
@@ -19,6 +19,6 @@ function getBooksOrMovies(){
     }
 
     const getBooksOrMoviesPromise = getBooksOrMovies();
-    getBooksOrMoviesPromise.then(results => console.log('getBooksOrMoviesPromise', results));
+    getBooksOrMoviesPromise.then(results => {console.log("getBooksOrMoviesPromise", results)});
 
 
