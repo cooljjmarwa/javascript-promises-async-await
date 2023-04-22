dcfc165fb7495d0f838ebe197ecd7abd1f43a1c8
cooljjjmarwa <cooljjjmarwa@gmail.com>
2023-04-22 11:47:37 +0300

    Completed module

--- src/services.js
+++ src/services.js
@@ -9,3 +9,23 @@ export function fetchMovies(){
         .catch(error => console.log(error));
 
 }
+
+export function fetchBooks(){
+    return fetch("./data/books.json")
+    .then(response => response.json())
+    .then(books => books)
+    .catch(error => console.log(error));
+
+}
+
+export  async function FetchMovies(){
+    try {
+        const response = await fetch('/data/movies.json');
+        const results = await response.json();
+        return results;
+    } catch (error) {
+        console.log(error);
+    }
+
+
+}
